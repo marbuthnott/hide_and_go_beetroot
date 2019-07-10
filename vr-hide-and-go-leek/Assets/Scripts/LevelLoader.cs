0910e8df00f9ef52129bd0e5ce79ec57a49da965
Student <magnusarbuthnott@gmail.com>
2019-07-10 16:15:11 +0100

    created interaction between the view pointer and an object
    Co-authored-by: n3ttl3t <n3ttl3t@gmail.com>
    Co-authored-by: marbuthnott <magnusarbuthnott@gmail.com>

---
+++ vr-hide-and-go-leek/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelLoader : MonoBehaviour
+{
+    void LoadLevel()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+}
