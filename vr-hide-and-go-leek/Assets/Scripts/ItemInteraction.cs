1f4262e227c7a9b937528c1acb648c8259beca39
VladC <vlad.chiriac24@gmail.com>
2019-07-10 16:16:50 +0100

    Add maze to scene

---
+++ vr-hide-and-go-leek/Assets/Scripts/ItemInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItems : MonoBehaviour
+{
+   public string Name;
+
+   public Sprite Image;
+
+   public string InteractText = "Tap once to interact with the item";
+
+   public virtual void OnInteract () {
+
+   }
+}
+
+
