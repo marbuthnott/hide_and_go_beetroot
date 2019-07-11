2e713471cc4860debc8f86473a92dc3428eb522d
VladC <vlad.chiriac24@gmail.com>
2019-07-11 12:12:40 +0100

    Add countdown timer
    Co-authored-by: n3ttl3t <n3ttl3t@gmail.com>

---
+++ vr-hide-and-go-leek/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+ 	
+ 	public int timeLeft = 60;
+ 	public Text countdownText;
+
+    void Start()
+    {
+		StartCoroutine("LoseTime");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("" + timeLeft);
+
+        if (timeLeft <= 0) {
+        	StopCoroutine("LoseTime");
+        	countdownText.text = "Time's up!";
+        }
+    }
+
+    IEnumerator LoseTime() {
+    	while (true) {
+    		yield return new WaitForSeconds(1);
+    		timeLeft--;
+    	}    	
+    }
+}
+ 
