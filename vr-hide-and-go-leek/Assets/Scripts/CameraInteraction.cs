1a66deaf279b9bd63f6c10287bf0bf7e45e26a98
VladC <vlad.chiriac24@gmail.com>
2019-07-12 16:33:57 +0100

    added play, easy, medium, hard button visibility on toggle
    Co-authored-by: marbuthnott <magnusarbuthnott@gmail.com>

---
+++ vr-hide-and-go-leek/Assets/Scripts/CameraInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInteraction : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+        	Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        	RaycastHit rayhit;
+        	if (Physics.Raycast(mouseray, out rayhit, 1000.0f)) {
+        		if (rayhit.transform.name == "GoSphere") {
+        			GameObject.Find("GoSphere").SetActive(false);
+
+        			GameObject.Find("EasySphere").GetComponent<MeshRenderer>().enabled = true;
+        			GameObject.Find("EasyTextSphere").GetComponent<MeshRenderer>().enabled = true;
+
+        			GameObject.Find("MediumSphere").GetComponent<MeshRenderer>().enabled = true;
+        			GameObject.Find("MediumTextSphere").GetComponent<MeshRenderer>().enabled = true;
+
+        			GameObject.Find("HardSphere").GetComponent<MeshRenderer>().enabled = true;
+        			GameObject.Find("HardTextSphere").GetComponent<MeshRenderer>().enabled = true;
+        		}
+        	}
+        }
+    }
+}
