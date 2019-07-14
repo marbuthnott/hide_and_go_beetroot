c339786c6ad06bbb0cf440fe9b295873a90fba4f
VladC <vlad.chiriac24@gmail.com>
2019-07-14 20:45:58 +0100

    Add time score for leaderboard
    Co-authored-by: n3ttl3t <n3ttl3t@gmail.com>
    Co-authored-by: Carlton Anand <carlton.anand@gmail.com>

--- vr-hide-and-go-leek/Assets/Scripts/ObjectInteraction.cs
+++ vr-hide-and-go-leek/Assets/Scripts/ObjectInteraction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Diagnostics;
 
 public class ObjectInteraction : MonoBehaviour
 {
@@ -10,12 +11,27 @@ public class ObjectInteraction : MonoBehaviour
     public CountdownTimer timer;
     public PlayerWalk walk;
 
+    private float startTime;
+    private float scoreTime;
+
+    // private Stopwatch stopWatch;
+
+
+    void Start()
+    {
+        startTime = Time.time;
+        // stopWatch = new Stopwatch();
+        // stopWatch.Start();
+    }
+
     void OnCollisionEnter()
     {
-        Debug.Log("Hit Object");
+        UnityEngine.Debug.Log("Hit Object");
         timer.enabled = false;
         hitObjectText.text = "You win!";
         walk.enabled = false;
+        UnityEngine.Debug.Log(Time.time);
+        calculateScoreTime();
         StartCoroutine("WinMessage");
     }
 
@@ -23,4 +39,15 @@ public class ObjectInteraction : MonoBehaviour
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene("EndMenu");
     }
+
+    void calculateScoreTime()
+    {
+        scoreTime = Time.time - startTime;
+        // scoreTime = scoreTime.ToString("F2");
+        // UnityEngine.Debug.Log(scoreTime.GetType());
+        // stopWatch.Stop();
+        // UnityEngine.Debug.Log(stopWatch.Elapsed);
+    }
+
+
 }
